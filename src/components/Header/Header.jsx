3c8a428045c0919706abe30e87c17dffc17bad2b
cassio <curriculocassio@hotmail.com>
2021-11-09 03:03:25 -0300

    algumas mudanças para SEO

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,10 +4,12 @@ import Navbar from "./Navbar";
 const Header = () => {
   return (
     <>
+    <header>
       <div className="flex justify-around items-center border-b border-gray-800 border-opacity-5 mt-5">
         <Logo />
         <Navbar />
       </div>
+    </header>
     </>
   );
 };
