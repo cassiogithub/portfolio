d8080befee3f239d47437a51e6e574bd54cfa64f
cassio <curriculocassio@hotmail.com>
2021-11-09 02:12:55 -0300

    yah more bug :d

--- src/components/myProjects/MyProjects.jsx
+++ src/components/myProjects/MyProjects.jsx
@@ -4,7 +4,7 @@ const MyProjects = () => {
   return (
     <>
       <section>
-        <div className="flex flex-col  justify-around items-center md:mt-56 xs:mt-10 bg-gray-900 text-white p-4">
+        <div className="flex flex-col  justify-around items-center mt-56 md:mt-56 xs:mt-10 bg-gray-900 text-white p-4">
           <span className="text-xl flex-nowrap">º</span>
             <div className="flex flex-wrap w-full justify-evenly mt-20">
             <TheProject description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur perspiciatis qui itaque eaque eligendi a quo id nam, eveniet numquam quaerat officia architecto quis possimus expedita est quod incidunt maxime." srcImg="/eu.jpg"/>
