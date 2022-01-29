955d6fbf242f2d31743c4d6990c31a983fdeafd8
Cassio R <curriculocassio@hotmail.com>
2022-01-29 10:36:18 -0300

    aniversario automatico

--- src/components/aboutMe/AboutMe.jsx
+++ src/components/aboutMe/AboutMe.jsx
@@ -1,26 +1,42 @@
 import SocialGroup from "../shared/SocialMediasGroup";
+import useState from "react";
 const AboutMe = () => {
+  function myAge(birth) {
+    let nowDate = new Date();
+    let nowYear = nowDate.getFullYear();
+    let birthParts = birth.split("/");
+    let dayBrith = birthParts[0];
+    let monthBirth = birthParts[1];
+    let yearBirth = birthParts[2];
+    let age = nowYear - yearBirth;
+    let monthNow = nowDate.getMonth() + 1;
+    if (monthNow < monthBirth) {
+      age--;
+    } else {
+      if (monthNow == monthBirth) {
+        if (new Date().getDate() < dayBrith) {
+          age--;
+        }
+      }
+    }
+
+    return age;
+  }
+
   return (
     <>
       <section id="AboutMe">
         <div className="flex justify-around mt-20 items-center flex-wrap-reverse">
-          <div
-            className="flex flex-col p-2"
-            data-aos="fade-right"
-            data-aos-duration="1000"
-          >
+          <div className="flex flex-col p-2" data-aos="fade-right" data-aos-duration="1000">
             <div className="flex">
               <div className="flex max-w-md items-center flex-col">
-                <h1 className="text-4xl self-start mb-2 ml-2 font-mono">
-                  Me conheça.
-                </h1>
+                <h1 className="text-4xl self-start mb-2 ml-2 font-mono">Me conheça.</h1>
                 <p className="text-gray-900 text-justify m-2 md:text-lg xs:text-sm xs:mt-2">
-                  Sou Cássio, tenho 25 anos, estou fazendo um curso técnico
-                  em informática, comecei a estudar programação  em novembro de
-                  2020, sempre usei a plataforma Udemy para estudos, hoje estou
-                  buscando com este portfólio me apresentar e dar entrada na
-                  área de programação, mesmo como estagiário , espero que tenham
-                  uma boa experiência com esta aplicação!  
+                  Sou Cássio, tenho {`${myAge("24/11/1995")} `}
+                  anos, estou fazendo um curso técnico em informática, comecei a estudar programação  em novembro de
+                  2020, sempre usei a plataforma Udemy para estudos, hoje estou buscando com este portfólio me
+                  apresentar e dar entrada na área de programação, mesmo como estagiário , espero que tenham uma boa
+                  experiência com esta aplicação!  
                 </p>
               </div>
             </div>
